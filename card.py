5cd60cce6714c3bdaa47d125ee769250bdc06702
Rebecca Gruver <Rebecca Gruver>
2016-10-01 14:31:24 -0400

    Terminal ver

--- card.py
+++ card.py
@@ -1,44 +1,19 @@
-import datetime
-import tkinter
-from tkinter import *
+def start():
+    print('(0) Decks\n(1) Add Deck\n')
+    cmd = in.next()
+    if cmd = '0':
+        decks()
+    if cmd = '1'
+        addDeck()
 
-class Card:
-    def __init__(self, front, back):
-        self.front = front
-        self.back = back
-        self.rating = 0
-        self.lastEdited = datetime.datetime.now()
-        self.reviewDay = datetime.datetime.now()
+def decks():
 
-class Deck:
-    def __init__(self, deckName):
-        self.name = deckName
-        self.cards = {}
+def loadDecks():
+    return ['Math', 'dsfjkl', 'sldkfj']
 
-class Study:
-    def __init__(self, root):
-        self.root = root
-        self.window = PanedWindow(self.root)
-        self.pMain()
+def loadCards(deck):
+    deck.cards = [card1, card2]
 
-    def pMain(self):
-        self.window.destroy()
-        self.window = PanedWindow(self.root)
-        self.window.pack()
-        bDecks = Button(self.window, text="Decks", command= lambda:self.pDecks())
-        bDecks.pack()
 
-    def pDecks(self):
-        self.window.destroy()
-        self.window = PanedWindow(self.root)
-        self.window.pack()
-        for d in decks:
-            b1 = Button(self.window, text=d.name, command= lambda d=d:self.pStudy(d))
-            b1.pack()
-
-    def pStudy(self, deck):
-        print(deck.name)
-
-decks = [Deck('hi'), Deck('Potato')]
-root = tkinter.Tk()
-s = Study(root)
+class deck:
+    
